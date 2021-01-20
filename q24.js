7f91bde03f27a2f9c5296b9bdbece57a94c742d8
eliav-touitou <77244738+eliav-touitou@users.noreply.github.com>
2021-01-20 17:11:56 +0200

    Update q24.js

--- q24.js
+++ q24.js
@@ -1,7 +1,9 @@
 function scan(arr) {
   let arr1 = [];
   for (let i = 0; i < arr.length; i++) {
-    if (!isNaN(arr[i]) && arr[i] != "" && arr[i] != null) arr1.push(arr[i]);
+    if (!isNaN(arr[i]) && arr[i] != "" && arr[i] != null) {
+      arr1.push(arr[i]);
+    }
   }
   return arr1;
 }

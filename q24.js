30b0d940f5de62a9eb2b49494831c29c12ef5cdc
eliav-touitou <77244738+eliav-touitou@users.noreply.github.com>
2021-01-20 16:42:57 +0200

    Create q24.js

---
+++ q24.js
@@ -0,0 +1,7 @@
+function scan(arr) {
+  let arr1 = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (!isNaN(arr[i]) && arr[i] != "" && arr[i] != null) arr1.push(arr[i]);
+  }
+  return arr1;
+}
